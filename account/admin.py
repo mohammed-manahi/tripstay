16757d147f8908f205a10e120da204b4c682187a
Mohammed Manahi <mohammedsaadmanahi@hotmail.com>
2023-02-03 21:08:37 +0300

    Create custom super user creation and register user in admin site

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from account.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """
+    Register customized user model in admin site
+    """
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                # Add user fields to default user model
+                "fields": ('email', 'first_name', 'last_name', 'role', 'password', 'username'),
+            },
+        ),
+    )
--- account/models.py
+++ account/models.py
@@ -9,10 +9,22 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=email, username=email, **extra_fields)
         user.set_password(password)
         user.save()
 
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and saves a superuser with the given email and password.
+        """
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=email, **extra_fields)
+        user.set_password(password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
         return user
 
 
